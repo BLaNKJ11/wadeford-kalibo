c42939fc6202e81b085e47383a6d8ddb6ad78b67
BLANKJ11 <aihayasaka1412@gmail.com>
2025-03-12 14:22:17 +0800

    change about page school hymn

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -45,6 +45,59 @@ export default function About() {
         </div>
       </div>
 
+      {/* Wadeford School Hymn Section */}
+      <div className="w-full bg-[#040873] text-white p-6 shadow-lg">
+        <h2 className="container mx-auto text-2xl font-semibold mb-4 ">WADEFORD SCHOOL HYMN</h2>
+            
+        <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-6 items-start">
+          <div className="space-y-2">
+            
+            <p>I</p>
+            <p>Riding on the cresting waves</p>
+            <p>With banners high, its flag unfurled</p>
+            <p>Sailing on from dawn to dawn</p>
+            <p>Towards a great and bright horizon</p>
+            <p>Waves we&apos;ve conquered, streams we&apos;ve forded</p>
+            <p>Together as principled Christian leaders</p>
+            <p>Wisdom toiled for will live forever</p>
+            <p>Hail, O hail our dear Alma Mater</p>
+          </div>
+          <div className="space-y-2">
+            
+            <p>II</p>
+            <p>Hail forever, Wadeford School</p>
+            <p>Noble, strong, steadfast and true</p>
+            <p>Let your banners proudly fly</p>
+            <p>Shining high, the gold and blue.</p>
+            <p>Live on, sail on, ford life&apos;s streams</p>
+            <p>Fill your sons&apos; and daughters&apos; dreams</p>
+            <p>CHORUS</p>
+            <p>Facere et Docere, Facere et Docere</p>
+            <p>Learning by example, living by its principle</p>
+            <p>Your legacy, our character</p>
+            <p>Wadeford School, we&apos;ve built together</p>
+            <p>Hail forever, Beloved Alma Mater!</p>
+          </div>
+          <div className="space-y-2">
+            <p>III</p>
+            <p>Inspired by the life and noble deeds</p>
+            <p>Of St. Jose Maria, our patron saint</p>
+            <p>Virtues to live by, examples shown</p>
+            <p>Values formed, a society transformed</p>
+            <p>Adversities we&apos;ll strive to conquer</p>
+            <p>Together as principled Christian leaders</p>
+            <p>Wisdom toiled for will live forever</p>
+            <p>Hail, O Wadeford, our Alma Mater</p>
+            <p>(Repeat Chorus)</p>
+          </div>
+        </div>
+        <div className="container mx-auto flex justify-center items-center mt-6">
+          <video controls className="rounded-lg shadow-lg w-full h-auto">
+            <source src="/assets/Wadeford_Hymn.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+      </div>
     </div>
   );
 }
